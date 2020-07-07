af2477ee09c31ec108156978817c916f65bcf538
石井葵 <62461941+IshiiAoi@users.noreply.github.com>
2020-07-07 18:42:19 +0900

    1-6
    配列・オブジェクト

---
+++ JavaScript/1-6/main.js
@@ -0,0 +1,15 @@
+// 問1
+let scores = [10, 15, 20, 25];
+for (let i = 0; i < scores.length; i++) {
+  if (scores[i] % 2 === 0) {
+    console.log(scores[i]);
+  }
+}
+
+// 問2
+let car = {
+  gasoline:1000 ,
+  number:1111 ,
+}
+console.log(car.gasoline);
+console.log(car.number);
