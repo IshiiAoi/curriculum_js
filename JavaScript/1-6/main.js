f388824fc50c0ea7449b50ff6e443facf83dd377
石井葵 <62461941+IshiiAoi@users.noreply.github.com>
2020-07-09 03:20:08 +0900

    Update main.js

--- JavaScript/1-6/main.js
+++ JavaScript/1-6/main.js
@@ -1,15 +1,18 @@
 // 問1
 let scores = [10, 15, 20, 25];
+console.log(scores);
 for (let i = 0; i < scores.length; i++) {
   if (scores[i] % 2 === 0) {
-    console.log(scores[i]);
+    console.log(`${scores[i]}は偶数です`);
   }
+  
 }
 
 // 問2
 let car = {
-  gasoline:1000 ,
-  number:1111 ,
+  gass: 20.5,
+  num: 1234,
 }
-console.log(car.gasoline);
-console.log(car.number);
+console.log(car);
+console.log(`ガソリンは${car.gass}です`);
+console.log(`ナンバーは${car.num}です`);
